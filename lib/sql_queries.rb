3874639ea36d3e62c5362fc6f7df15aeca94de31
Walker Le <walker.le88@gmail.com>
2023-08-08 23:03:09 -0600

    revert

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,19 +1,19 @@
 def selects_all_female_bears_return_name_and_age
-  "SELECT bears.name, bears.age FROM bears WHERE sex='F';"
+  "Write your SQL query here"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "SELECT name FROM bears ORDER BY name ASC;"
+  "Write your SQL query here"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "SELECT name, age FROM bears WHERE alive = 1 ORDER BY age ASC;"
+  "Write your SQL query here"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT name, age FROM bears ORDER BY age DESC LIMIT 1;"
+  "Write your SQL query here"
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT name, age FROM bears ORDER BY age LIMIT 1;"
+  "Write your SQL query here"
 end
