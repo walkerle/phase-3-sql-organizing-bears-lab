c4126c32b3fafb55ef148e65a6a2bd4953ad68ff
Walker Le <walker.le88@gmail.com>
2023-08-09 15:16:36 -0600

    complete

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  sex TEXT, --CHAR(1) also works
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr.Chocolate', 2, 'M', 'Brown', 'Mild', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 2, 'M', 'Brown', 'Mild', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 2, 'M', 'Brown', 'Mild', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 2, 'M', 'Brown', 'Mild', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 2, 'M', 'Brown', 'Mild', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 2, 'M', 'Brown', 'Mild', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 2, 'M', 'Brown', 'Mild', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 2, 'M', 'Brown', 'Mild', true);
