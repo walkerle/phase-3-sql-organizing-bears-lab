601a47b10ce49e1bba892a99d845b08af1849bfb
Walker Le <walker.le88@gmail.com>
2023-08-08 22:54:20 -0600

    bootcamp

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, sex) VALUES ("Mr. Chocolate", "M");
+INSERT INTO bears (name, sex) VALUES ("Rowdy", "M");
+INSERT INTO bears (name, sex) VALUES ("Tabitha", "M");
+INSERT INTO bears (name, sex) VALUES ("Sergeant Brown", "M");
+INSERT INTO bears (name, sex) VALUES ("Melissa", "M");
+INSERT INTO bears (name, sex) VALUES ("Grinch", "M");
+INSERT INTO bears (name, sex) VALUES ("Wendy", "M");
+INSERT INTO bears (name, sex) VALUES (NULL, "M");
